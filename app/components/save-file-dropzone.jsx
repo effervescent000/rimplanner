d7b7df5ff8ca9f4fa534771c41a3966ac31601cf
effervescent000 <kislio01@gmail.com>
2022-08-26 09:59:04 -0400

    fixed xml parsing

--- app/components/save-file-dropzone.jsx
+++ app/components/save-file-dropzone.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useCallback, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
 import { useOutletContext } from "@remix-run/react";
@@ -10,17 +11,17 @@ const SaveFileDropzone = () => {
   const { setSaveData } = useOutletContext();
 
   useEffect(() => {
-    console.log(parseString(rawSave));
+    parseString(rawSave, (err, result) => setSaveData(result));
     setProcessing(false);
   }, [rawSave]);
 
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
+      setProcessing(true);
       const reader = new FileReader();
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        setProcessing(true);
         setRawSave(reader.result);
       };
       reader.readAsText(file);
