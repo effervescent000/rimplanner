eab2e0c74213d962308d0eff39b33231c5bf3e77
effervescent000 <kislio01@gmail.com>
2022-08-27 10:06:22 -0400

    added functions to get basic statistics about pawns

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,3 +1,20 @@
 export const PLAYER_COLONY = "PlayerColony";
 export const PAWN_CONSTANT = "Pawn";
 export const HUMAN_CONSTANT = "Human";
+export const MINOR_PASSION = "Minor";
+export const MAJOR_PASSION = "Major";
+
+export const SKILLS = [
+  "Shooting",
+  "Melee",
+  "Construction",
+  "Mining",
+  "Cooking",
+  "Plants",
+  "Animals",
+  "Crafting",
+  "Artistic",
+  "Medicine",
+  "Social",
+  "Intellectual",
+];
---
+++ app/helpers/rosterHelpers.js
@@ -0,0 +1,61 @@
+import { mean, quantile } from "simple-statistics";
+
+import { MAJOR_PASSION, MINOR_PASSION, SKILLS } from "~/constants/constants";
+
+const buildColonyStats = (pawns) => {
+  let allSkills = {};
+  pawns.forEach(
+    ({
+      skills: {
+        skills: { li: skills },
+      },
+    }) => {
+      skills.forEach((skill) => {
+        allSkills = {
+          ...allSkills,
+          [skill.def]: allSkills[skill.def]
+            ? [...allSkills[skill.def], { level: skill.level, passion: skill.passion }]
+            : [{ level: skill.level, passion: skill.passion }],
+        };
+      });
+    }
+  );
+  const stats = SKILLS.map((skill) => ({
+    name: skill,
+    average: mean(allSkills[skill].map(({ level }) => +level || 0)),
+    upperQuantile: quantile(
+      allSkills[skill].map(({ level }) => +level || 0),
+      0.75
+    ),
+  }));
+  return stats.reduce((total, cur) => ({ ...total, [cur.name]: cur }), {});
+};
+
+export const buildRosterHighlights = (playerPawns) => {
+  let stats = {};
+  const colonyStats = buildColonyStats(playerPawns);
+  playerPawns.forEach(
+    ({
+      name: { nick: name },
+      skills: {
+        skills: { li: skills },
+      },
+    }) => {
+      stats = {
+        ...stats,
+        [name]: {
+          highlights: skills.map(({ def: skill, level, passion }) => {
+            if (
+              level >= colonyStats[skill].upperQuantile ||
+              (level >= colonyStats[skill].upperQuantile - 2 && passion === MINOR_PASSION) ||
+              (level >= colonyStats[skill].upperQuantile - 5 && passion === MAJOR_PASSION)
+            )
+              return { skill, level, passion };
+            return undefined;
+          }),
+        },
+      };
+    }
+  );
+  return stats;
+};
