12ecd12c11e3873f89bed831c4049b9eee90f13d
effervescent000 <kislio01@gmail.com>
2022-09-03 07:45:07 -0400

    moved eval stuff directly into eval route

--- app/routes/eval/index.jsx
+++ app/routes/eval/index.jsx
@@ -1,7 +1,25 @@
-import Evaluation from "~/components/evaluation/evaluation";
+import { useState } from "react";
+import { useOutletContext } from "@remix-run/react";
+
+import ControlledTextInput from "~/components/common/controlled-text-input";
+import PawnCard from "~/components/common/pawn-card";
 
 const EvaluationIndex = () => {
-  return <Evaluation />;
+  const {
+    saveData: { worldPawns },
+  } = useOutletContext();
+  const [search, setSearch] = useState("");
+
+  return (
+    <div>
+      <ControlledTextInput value={search} callback={setSearch} />
+      {worldPawns
+        .filter(({ name: { nick } }) => nick && nick.toLowerCase().includes(search.toLowerCase()))
+        .map((pawn) => (
+          <PawnCard key={pawn.id} pawn={pawn} />
+        ))}
+    </div>
+  );
 };
 
 export default EvaluationIndex;
