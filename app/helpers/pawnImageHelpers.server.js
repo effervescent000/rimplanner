fe4c06b7786865e30fcffa618e7f785f80235f6f
effervescent000 <kislio01@gmail.com>
2022-09-14 18:17:51 -0400

    error logging

--- app/helpers/pawnImageHelpers.server.js
+++ app/helpers/pawnImageHelpers.server.js
@@ -68,7 +68,7 @@ export const composeImage = async ({
     const url = await baseImage.getBase64Async(jimp.MIME_PNG);
     return url;
   } catch (baseError) {
-    // console.log(baseError);
+    console.log(baseError);
     return "";
   }
 };
