df911b619c45a70a425f7024d8a02e09478c3bf3
effervescent000 <kislio01@gmail.com>
2022-11-06 09:03:32 -0500

    display groups on group maker page

--- app/routes/groups.tsx
+++ app/routes/groups.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useFetcher } from "@remix-run/react";
 
 import RWContext from "../context/RWContext";
 
-import type { RimContextParams } from "../types/interfaces";
+import type { PawnParams, RimContextParams } from "../types/interfaces";
+import PawnCard from "app/components/common/pawn-card";
 
 const GroupMakerIndex = () => {
+  const [groups, setGroups] = useState({
+    home: [] as Array<PawnParams>,
+    away: [] as Array<PawnParams>,
+  });
   const fetcher = useFetcher();
   const {
     saveData: { colonists, slaves, prisoners, modList },
@@ -14,18 +19,50 @@ const GroupMakerIndex = () => {
   }: RimContextParams = useContext(RWContext);
 
   useEffect(() => {
-    fetcher.submit({
-      values: JSON.stringify({
-        colonists, prisoners, slaves, config, modList
-      })
-    }, {method: "post", action: "/groups/builder"})
+    fetcher.submit(
+      {
+        values: JSON.stringify({
+          colonists,
+          prisoners,
+          slaves,
+          config,
+          modList,
+        }),
+      },
+      { method: "post", action: "/groups/builder" }
+    );
   }, [colonists, slaves, prisoners, config]);
 
-  return ( 
-    <div>
-      Nothing here yet, sorry!
-    </div>
-   );
-}
- 
+  useEffect(() => {
+    if (fetcher.type === "done") {
+      setGroups(fetcher.data.groups);
+    }
+  }, [fetcher.type]);
+
+  return (
+    <>
+      {groups.home.length || groups.away.length ? (
+        <div>
+          <div>
+            <div>Home group</div>
+            <div className="flex">
+              {groups.home.map((pawn) => (
+                <PawnCard pawn={pawn} key={pawn.id} />
+              ))}
+            </div>
+          </div>
+          <div>Away group</div>
+          <div className="flex">
+            {groups.away.map((pawn) => (
+              <PawnCard pawn={pawn} key={pawn.id} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
 export default GroupMakerIndex;
